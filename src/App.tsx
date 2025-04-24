152d286e45bed3860d84e54c7bd741bbe81b7a45
Zihao Jiang <77188419+zihaojng@users.noreply.github.com>
2025-04-24 19:21:30 -0400

    [Magic Patterns] Updated from 'Increase Logo Size to 128px'

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Flyer } from './components/Flyer';
+export function App() {
+  return <div className="min-h-screen bg-gray-100">
+      <Flyer />
+    </div>;
+}
